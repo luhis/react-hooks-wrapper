7fa2f3a6112a6abd7cb5464493b5da3725dbefb9
Matthew McCorry <mccorry@gmail.com>
2018-12-31 19:37:59 +0000

    removed extra type param

--- TypeFunctions.ts
+++ TypeFunctions.ts
@@ -2,10 +2,10 @@ import { ReactNode } from "react";
 
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-export const ReconstituteFunctionComponent: <TFinal extends TOther, TOther extends object, TDontCare extends object>
-    (props: Omit<TFinal, keyof (TOther)> & TDontCare & { children?: ReactNode }, other: TOther) => TFinal =
-    <TFinal extends TOther, TOther extends object, TDontCare extends object>
-        (props: Omit<TFinal, keyof (TOther)> & TDontCare & { children?: ReactNode }, other: TOther) => {
+export const ReconstituteFunctionComponent: <TFinal extends TOther, TOther extends object>
+    (props: Omit<TFinal, keyof (TOther)> & { children?: ReactNode }, other: TOther) => TFinal =
+    <TFinal extends TOther, TOther extends object>
+        (props: Omit<TFinal, keyof (TOther)> & { children?: ReactNode }, other: TOther) => {
         return { ...props, ...other } as unknown as TFinal;
     };
 
--- __tests__/Reconstitute.spec.ts
+++ __tests__/Reconstitute.spec.ts
@@ -2,8 +2,7 @@ import { ReconstituteFunctionComponent } from "../TypeFunctions";
 
 test("reconstitute should", () => {
   type extras = { c: number };
-  type final = { a: number, b: number, c: string } & extras & other;
-  type other = { z: string };
-  const res: final = ReconstituteFunctionComponent<final, extras, other>({ a: 1, b: 2, z: "z" }, { c: 3 });
-  expect(res).toEqual(<final>{ a: 1, b: 2, c: 3, z: "z" });
+  type final = { a: number, b: number, c: string } & extras;
+  const res: final = ReconstituteFunctionComponent<final, extras>({ a: 1, b: 2 }, { c: 3 });
+  expect(res).toEqual(<final>{ a: 1, b: 2, c: 3});
 });
