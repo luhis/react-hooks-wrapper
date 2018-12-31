f4be268a681bdff7f69c30b61cc16138d12530ee
Matthew McCorry <mccorry@gmail.com>
2018-12-31 19:09:04 +0000

    allowed state to be a number

--- StateWrapper.ts
+++ StateWrapper.ts
@@ -3,12 +3,12 @@ import { useState, FunctionComponent, ReactNode } from "react";
 import SetState from "./SetState";
 import { Omit, ReconstituteFunctionComponent, TupleToObject } from "./TypeFunctions";
 
-type TypeDef = <TState extends object, TTransformed extends object, TNeedsProps extends object>
+type TypeDef = <TState, TTransformed extends object, TNeedsProps extends object>
     (defaultVal: TState, mapTupleToProps: TupleToObject<[TState, SetState<TState>], TTransformed>) =>
     <P extends TTransformed >(component: FunctionComponent<P>) => FunctionComponent<Omit<P, keyof (TTransformed)> & TNeedsProps>;
 
 const StateWrapper: TypeDef =
-    <TState extends object, TTransformed extends object, TNeedsProps extends object>
+    <TState, TTransformed extends object, TNeedsProps extends object>
         (defaultVal: TState, mapTupleToProps: TupleToObject<[TState, SetState<TState>], TTransformed>) =>
         <P extends TTransformed>(component: FunctionComponent<P>) =>
             (props: Omit<P, keyof (TTransformed)> & TNeedsProps & { children?: ReactNode }) => {
