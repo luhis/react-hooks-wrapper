31927d6b9f287fd7f9fffab70a9a77fb980a2010
Matthew McCorry <mccorry@gmail.com>
2019-01-01 10:42:37 +0000

    Released new version and updated the examples to use it

--- Examples/src/App.tsx
+++ Examples/src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
+import Effect from "./Effect";
+import Reducer from "./Reducer";
 import State from "./State";
 
 const App: React.FunctionComponent = () => {
     return (
       <>
-        <State name="matt from App.js" />
+        State:
+        <State name="matt from App.tsx" />
+        Effect:
+        <Effect />
+        Reducer:
+        <Reducer name="hello from app.tsx"/>
       </>
     );
 };
