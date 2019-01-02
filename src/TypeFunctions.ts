2eaae762984d57c3ff1967aef1518186fe98670e
Matthew McCorry <mccorry@gmail.com>
2019-01-02 22:12:27 +0000

    fixed type error

--- src/TypeFunctions.ts
+++ src/TypeFunctions.ts
@@ -2,10 +2,12 @@ import { FunctionComponent, ReactNode } from "react";
 
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
+interface IReactChildren { readonly children?: ReactNode; }
+
 export const ReconstituteProps =
     <TFinal extends TOther, TOther extends object>
-        (props: Omit<TFinal, keyof (TOther)> & { readonly children?: ReactNode }, other: TOther) =>
-        ({ ...props, ...other } as unknown as TFinal);
+        (props: Omit<TFinal, keyof (TOther)> & IReactChildren, other: TOther) =>
+        ({ ...props, ...other } as (TFinal & IReactChildren));
 
 export type TupleToObject<TLeft, TRight, TOut extends object> = (_: [TLeft, (_: TRight) => void]) => TOut;
 
