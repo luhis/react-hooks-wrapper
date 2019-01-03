41d44c627b07f55bc98be3148a6d1651d89262da
Matthew McCorry <mccorry@gmail.com>
2019-01-03 23:22:58 +0000

    added test with child nodes

---
+++ __tests__/WIthChildren.spec.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import * as renderer from "react-test-renderer";
+
+import { setState, stateWrapper } from "../src/Index";
+
+interface IFinalComponentProps { readonly name: string; readonly children?: React.ReactNode }
+type Props = IFinalComponentProps & { readonly count: number; readonly setCount: setState<number> };
+
+const Counter: React.FunctionComponent<Props> = ({ count, setCount, name, children }) =>
+    <div>
+        <p>Hi {name}, You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+            Click me
+      </button>
+      {children}
+    </div>;
+
+const CounterContainer: React.FunctionComponent<IFinalComponentProps>
+    = stateWrapper(1, ([count, setCount]) => ({ count, setCount }))(Counter);
+
+test("WithChildren should", () => {
+    const myComponent = renderer.create(
+    <CounterContainer name="matt">
+        <span>hello world</span>
+        <span>hello world 2</span>
+    </CounterContainer>).toJSON();
+    expect(myComponent).toMatchSnapshot();
+});
