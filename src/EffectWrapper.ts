4e704371ac698ac1ddbd9da63d5fe016b56579f0
Matthew McCorry <mccorry@gmail.com>
2019-02-17 11:45:16 +0000

    added effect trigger filtering

--- src/EffectWrapper.ts
+++ src/EffectWrapper.ts
@@ -3,10 +3,10 @@ import { createElement, FunctionComponent, ReactNode, useEffect } from "react";
 type EffectResponse = void | (() => void);
 
 export default
-    <TProps extends object>(effectFuc: (props: TProps) => EffectResponse) =>
+    <TProps extends object>(effectFuc: (props: TProps) => EffectResponse, inputs?: ReadonlyArray<any> | undefined) =>
         (component: FunctionComponent<TProps>) => {
             const EffectWrapper =  (props: TProps & { readonly children?: ReactNode } ) => {
-                useEffect(() => effectFuc(props));
+                useEffect(() => effectFuc(props), inputs);
                 return createElement(component, props);
             };
 
